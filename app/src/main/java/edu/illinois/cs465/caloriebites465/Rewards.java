ca386e93fd79309b9109c992d47d471988f3f119
Prashant Naganaboyina <pnaganab@gmail.com>
2022-11-10 16:46:13 -0600

    Edit List View

--- app/src/main/java/edu/illinois/cs465/caloriebites465/Rewards.java
+++ app/src/main/java/edu/illinois/cs465/caloriebites465/Rewards.java
@@ -3,12 +3,24 @@ package edu.illinois.cs465.caloriebites465;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Rewards extends AppCompatActivity {
+
+    ListView pastRecordsView;
+
+    //change to get dates dynamically
+    String pastRecords[] = {"November 10th", "November 9th", "November 8th", "November 7th", "November 6th", "November 5th"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rewards);
+
+        pastRecordsView = (ListView)findViewById(R.id.pastRecordsList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_rewards, R.id.textView, pastRecords);
+        System.out.println(arrayAdapter);
+        pastRecordsView.setAdapter(arrayAdapter);
     }
 }
