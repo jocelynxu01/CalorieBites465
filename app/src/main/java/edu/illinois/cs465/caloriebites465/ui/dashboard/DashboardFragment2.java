e0e9cc80b07133df51d8f573a3abd0864a7cae24
JamesCao0326 <yuchenc5@illinois.edu>
2022-11-10 16:25:45 -0600

    Added calorie converter page.

---
+++ app/src/main/java/edu/illinois/cs465/caloriebites465/ui/dashboard/DashboardFragment2.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs465.caloriebites465.ui.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardFragment2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_dashboard2);
+    }
+}
