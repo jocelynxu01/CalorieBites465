9cb20f361bfad5a1bb83150dc2ee624d16d2aa31
jocelynxu01 <jocelynxu01@gmail.com>
2022-11-10 14:40:56 -0600

    home page almost done

--- app/src/main/java/edu/illinois/cs465/caloriebites465/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/edu/illinois/cs465/caloriebites465/ui/dashboard/DashboardFragment.java
@@ -24,8 +24,6 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
