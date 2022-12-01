baa990bd07de1f9789aaf36fec6b222706568ae7
JamesCao0326 <yuchenc5@illinois.edu>
2022-12-01 17:56:09 -0600

    Added search functionality in the calorie page.

--- app/src/main/java/edu/illinois/cs465/caloriebites465/Converter.java
+++ app/src/main/java/edu/illinois/cs465/caloriebites465/Converter.java
@@ -1,13 +1,7 @@
 package edu.illinois.cs465.caloriebites465;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
