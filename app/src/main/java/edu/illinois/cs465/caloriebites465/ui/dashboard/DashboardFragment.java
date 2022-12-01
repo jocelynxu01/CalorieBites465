72a36eeefdfa66eca061c131c9513a20775a63f8
JamesCao0326 <yuchenc5@illinois.edu>
2022-12-01 12:43:22 -0600

    Added search functionality in converter page.

--- app/src/main/java/edu/illinois/cs465/caloriebites465/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/edu/illinois/cs465/caloriebites465/ui/dashboard/DashboardFragment.java
@@ -9,27 +9,30 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+
+import java.util.Random;
 
 import edu.illinois.cs465.caloriebites465.Converter;
 import edu.illinois.cs465.caloriebites465.R;
-import edu.illinois.cs465.caloriebites465.Rewards;
 import edu.illinois.cs465.caloriebites465.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
     String[] item = {"Apples", "Oranges", "Bananas", "Hamburgers", "Pizza Slice", "PBJ Sandwich", "Oreo Cookies", "Potato Chips"};
 
     AutoCompleteTextView autoCompleteTextView;
 
     ArrayAdapter<String> adapterItems;
+
+    String input;
+
+    private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -47,8 +50,8 @@ public class DashboardFragment extends Fragment {
 
         autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String item = adapterView.getItemAtPosition(position).toString();
+            public void onItemClick(AdapterView<?> adapterView, View v, int position, long l) {
+                input = adapterView.getItemAtPosition(position).toString();
             }
         });
 
@@ -62,7 +65,34 @@ public class DashboardFragment extends Fragment {
             }
         });
 
+        Button btn = (Button) root.findViewById(R.id.searchButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleNumber(v);
+                handleFood(v, input);
+                randomize(v);
+            }
+        });
         return root;
+    }
+
+    public void handleNumber(View v) {
+        EditText t = getView().findViewById(R.id.input_number);
+        String input = t.getText().toString();
+        ((TextView) getView().findViewById(R.id.output_number)).setText(input);
+    }
+
+    public void handleFood(View v, String input) {
+        ((TextView) getView().findViewById(R.id.output_food)).setText(input.toLowerCase());
+    }
+
+    public void randomize(View v) {
+        int steps = new Random().nextInt(4001) + 2000; // [0, 4000] + 2000 => [2000, 6000]
+        int calories = new Random().nextInt(901) + 100; // [0, 900] + 100 => [100, 1000]
+        ((TextView) getView().findViewById(R.id.numSteps)).setText(String.valueOf(steps));
+        ((TextView) getView().findViewById(R.id.numCalories)).setText(String.valueOf(calories));
     }
 
     @Override
