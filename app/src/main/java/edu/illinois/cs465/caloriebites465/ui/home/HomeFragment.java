d761c0424228ead3e50af1e44798b7e013029a79
jocelynxu01 <jocelynxu01@gmail.com>
2022-12-02 16:51:37 -0600

    shuffle button starting stage

--- app/src/main/java/edu/illinois/cs465/caloriebites465/ui/home/HomeFragment.java
+++ app/src/main/java/edu/illinois/cs465/caloriebites465/ui/home/HomeFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -12,11 +16,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.util.Random;
+
 import edu.illinois.cs465.caloriebites465.R;
 import edu.illinois.cs465.caloriebites465.SettingsActivity;
+import edu.illinois.cs465.caloriebites465.databinding.FragmentDashboardBinding;
 import edu.illinois.cs465.caloriebites465.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+    String[] item = {"Apples", "Oranges", "Bananas", "Hamburgers", "Pizza Slice", "PBJ Sandwich", "Oreo Cookies", "Potato Chips"};
 
     private FragmentHomeBinding binding;
 
@@ -39,7 +48,27 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        // search
+        ImageButton btn = (ImageButton) root.findViewById(R.id.shufflebutton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleFood(v);
+                randomize(v);
+            }
+        });
+
         return root;
+    }
+
+    public void handleFood(View v) {
+        int random = new Random().nextInt(item.length);
+    }
+
+    public void randomize(View v) {
+        int number = new Random().nextInt(11) + 1; // [0, 4000] + 2000 => [2000, 6000]
+        ((TextView) getView().findViewById(R.id.foodnumber)).setText(String.valueOf(number));
     }
 
     @Override
